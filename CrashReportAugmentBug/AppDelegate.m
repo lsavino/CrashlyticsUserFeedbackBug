808759fb4ce4cff8791f0124b34997c64179ad61
Laura Savino <savino@adobe.com>
2020-09-24 13:47:06 -0700

    Initial Commit

---
+++ CrashReportAugmentBug/AppDelegate.m
@@ -0,0 +1,50 @@
+//
+//  ADOBE CONFIDENTIAL
+//  __________________
+//
+//  Copyright 2020 Adobe 
+//  All Rights Reserved.
+//
+//  NOTICE:  All information contained herein is, and remains
+//  the property of Adobe and its suppliers, if any. The intellectual
+//  and technical concepts contained herein are proprietary to Adobe
+//  and its suppliers and are protected by all applicable intellectual
+//  property laws, including trade secret and copyright laws.
+//  Dissemination of this information or reproduction of this material
+//  is strictly forbidden unless prior written permission is obtained
+//  from Adobe.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+	// Override point for customization after application launch.
+	return YES;
+}
+
+
+#pragma mark - UISceneSession lifecycle
+
+
+- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
+	// Called when a new scene session is being created.
+	// Use this method to select a configuration to create the new scene with.
+	return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+}
+
+
+- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
+	// Called when the user discards a scene session.
+	// If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+	// Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+}
+
+
+@end
